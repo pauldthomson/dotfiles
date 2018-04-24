437c5ee3c75abcab184f24efa9c918d292ad6fd8
Paul Thomson <thomsonp83@gmail.com>
2018-04-24 15:46:46 +1000

    Add hombrew script

---
+++ homebrew.sh
@@ -0,0 +1,41 @@
+#core
+brew install coreutils
+
+# key commands
+brew install binutils
+brew install diffutils
+brew install ed --default-names
+brew install findutils --with-default-names
+brew install the_silver_searcher
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install watch
+brew install wget
+
+# OS X ships a GNU version, but too old
+brew install bash
+brew install emacs
+brew install gdb  # gdb requires further actions to make it work. See `brew info gdb`.
+brew install gpatch
+brew install m4
+brew install make
+brew install nano
+
+# Other commands (non-GNU)
+brew install file-formula
+brew install git
+brew install less
+brew install openssh
+brew install python
+brew install rsync
+brew install unzip
+brew install vim --override-system-vi
+brew install jenv
+brew install eaven
+brew install ant
+brew install telnet
