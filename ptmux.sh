0c3cc8d65677523a65c8a068543e6833b0e30125
Paul Thomson <thomsonp83@gmail.com>
2018-05-07 10:53:22 +1000

    Start trying to handle multiple results

--- ptmux.sh
+++ ptmux.sh
@@ -5,8 +5,18 @@ PROJECT=$1
 tmux new -d -s $PROJECT
 
 DIRECTORY=`find ~/code -maxdepth 2 -type d -name $PROJECT`
-if [ -z $DIRECTORY ]; then
-		echo Project not found, you may need to clone it 
+echo $DIRECTORY
+NUMBER=1
+if [ "${#DIRECTORY[@]}" == 0 ]; then
+		printf "Project not found, you may need to clone it" 
+elif [ "${#DIRECTORY[@]}" > 1 ]; then
+		printf "Which directory do you want?\n"
+		for dir in "${DIRECTORY[@]}"
+		do
+				echo "$NUMBER. $dir"
+				(( NUMBER++ ))
+				echo $NUMBER
+		done
 else
 		tmux send-keys -t $PROJECT "cd $DIRECTORY" ENTER
 fi
