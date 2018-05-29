29011861ffaf58e5a859a0cc8a01bdd66e5a0a2a
Paul Thomson <thomsonp83@gmail.com>
2018-05-30 09:45:31 +1000

    CD to home directory when project not found in ptmux script

--- ptmux.sh
+++ ptmux.sh
@@ -7,6 +7,7 @@ tmux new -d -s $PROJECT
 DIRECTORY=`find ~/code -maxdepth 2 -type d -name $PROJECT`
 if [ -z $DIRECTORY ]; then
 		echo Project not found, you may need to clone it 
+		tmux send-keys -t $PROJECT "cd" ENTER
 else
 		tmux send-keys -t $PROJECT "cd $DIRECTORY" ENTER
 fi
