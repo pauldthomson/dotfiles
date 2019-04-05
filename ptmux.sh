252c4569b02d6b20cc64c3dc2fe11b21436b426c
Paul Thomson <pthomson@tyro.com>
2019-04-05 20:12:22 +1100

    Add shortcuts for fzf actions and enhance ptmux
    Ctrl-s now calls tm function. Ctrl-t opens files in vim. Ptmux script
    goes to newly created tmux session

--- ptmux.sh
+++ ptmux.sh
@@ -4,10 +4,18 @@ PROJECT=$1
 
 tmux new -d -s $PROJECT
 
-DIRECTORY=`find ~/code -maxdepth 2 -type d -name $PROJECT`
-if [ -z $DIRECTORY ]; then
-		echo Project not found, you may need to clone it 
-		tmux send-keys -t $PROJECT "cd" ENTER
+DIRECTORY=$(find ~/code -maxdepth 2 -type d -name $PROJECT)
+if [[ -z "$DIRECTORY" ]]; then
+	echo Project not found, you may need to clone it 
+	tmux send-keys -t $PROJECT "cd" ENTER
 else
-		tmux send-keys -t $PROJECT "cd $DIRECTORY" ENTER
+	tmux send-keys -t $PROJECT "cd $DIRECTORY" ENTER
+fi
+
+tmux send-keys -t $PROJECT "clear" ENTER
+
+if [[ "$TMUX" ]]; then
+	tmux switch -t $PROJECT
+else
+	tmux a -t $PROJECT
 fi
