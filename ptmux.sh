14276397f30d8b417911fb5c9ebbe423c8735f58
Paul Thomson <pthomson@tyro.com>
2019-05-23 14:25:09 +1000

    Update scripts to match new directory structure

--- ptmux.sh
+++ ptmux.sh
@@ -4,7 +4,7 @@ PROJECT=$1
 
 tmux new -d -s $PROJECT
 
-DIRECTORY=$(find ~/code -maxdepth 2 -type d -name $PROJECT)
+DIRECTORY=$(find ~/repos -maxdepth 3 -type d -name $PROJECT)
 if [[ -z "$DIRECTORY" ]]; then
 	echo Project not found, you may need to clone it 
 	tmux send-keys -t $PROJECT "cd" ENTER
