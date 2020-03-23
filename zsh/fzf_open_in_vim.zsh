a1aaae5ce42e95aeeed171df848746964fdbdc26
Paul Thomson <pthomson@tyro.com>
2020-03-24 06:24:01 +1100

    Add preview window to ctrl-t command

--- zsh/fzf_open_in_vim.zsh
+++ zsh/fzf_open_in_vim.zsh
@@ -1,6 +1,6 @@
 # Search a file with fzf inside a Tmux pane and then open it in an editor
 fzf_then_open_in_editor() {
-  local file=$(fzf-tmux)
+  local file=$(fzf-tmux --preview 'bat --style=numbers --color=always {}')
   # Open the file if it exists
   if [ -n "$file" ]; then
     # Use the default editor if it's defined, otherwise Vim
