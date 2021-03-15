cc0e63102268de14d69c0a7d2041e5d0aa3a50a6
Paul Thomson <pthomson@tyro.com>
2021-03-15 15:59:15 +1100

    Fix zsh widget

--- zsh/fzf_open_in_vim.zsh
+++ zsh/fzf_open_in_vim.zsh
@@ -1,6 +1,6 @@
 # Search a file with fzf inside a Tmux pane and then open it in an editor
 fzf_then_open_in_editor() {
-  local file=$(fzf-tmux --preview 'bat --style=numbers --color=always {}')
+  local file=$(fzf-tmux</dev/tty --preview 'bat --style=numbers --color=always {}')
   # Open the file if it exists
   if [ -n "$file" ]; then
     # Use the default editor if it's defined, otherwise Vim
@@ -8,9 +8,10 @@ fzf_then_open_in_editor() {
   fi
 }
 
-zle -N fzf_then_open_in_editor
+zle -N fzf_then_open_in_editor fzf_then_open_in_editor
 
 # This should work, needs more investigation
 # into fzf-tmux and zsh
-# bindkey "^T" fzf_then_open_in_editor
-bindkey -s '^T' 'fzf_then_open_in_editor\n'
+# Works now thanks to: https://unix.stackexchange.com/a/595386
+bindkey "^T" fzf_then_open_in_editor
+# bindkey -s '^T' 'fzf_then_open_in_editor\n'
