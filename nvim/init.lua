53fa04e48bb2b0d1f56c49511f5e3ddbe1cbaefa
Paul Thomson <pthomson@tyro.com>
2024-01-18 15:08:55 +1100

    Remove unneeded stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,3 @@
--- local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'ini
--- local is_bootstrap = false
--- if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
---   is_bootstrap = true
---   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
---   vim.cmd [[packadd packer.nvim]]
--- end
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
