94852b0cb103c90fd350645120b244a7db4a6e2d
Paul Thomson <pthomson@tyro.com>
2024-09-13 10:47:41 +1000

    Move lsp config

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -26,5 +26,127 @@ return { -- LSP Configuration & Plugins
     {
         'j-hui/fidget.nvim',
         opts = {}
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+            local servers = {
+                -- clangd = {},
+                gopls = {
+                    completeUnimported = true,
+                    usePlaceholders = true,
+                    analyses = {
+                        unusedparams = true,
+                    }
+                },
+                -- pyright = {},
+                -- rust_analyzer = {},
+                -- tsserver = {},
+                jdtls = {},
+                kotlin_language_server = {},
+                yamlls = {
+                    yaml = {
+                        keyOrdering = false,
+                        schemaStore = {
+                            enable = true,
+                        },
+                        schemas = {
+                            ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/master/all.json"] =
+                            "/*",
+                        },
+                    },
+                },
+                terraformls = {},
+                diagnosticls = {},
+                lua_ls = {
+                    Lua = {
+                        workspace = { checkThirdParty = false },
+                        telemetry = { enable = false },
+                    },
+                },
+                omnisharp = {},
+                omnisharp_mono = {},
+                csharp_ls = {},
+                bashls = {},
+                marksman = {},
+            }
+
+            local on_attach = function(client, bufnr)
+                -- NOTE: Remember that lua is a real programming language, and as such it is possible
+                -- to define small helper and utility functions so you don't have to repeat yourself
+                -- many times.
+                --
+                -- In this case, we create a function that lets us more easily define mappings specific
+                -- for LSP related items. It sets the mode, buffer and description for us each time.
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
+
+                    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+                end
+
+                nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+                nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+                nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+                -- See `:help K` for why this keymap
+                nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+                vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help,
+                    { buffer = bufnr, desc = 'Signature Documentation' })
+
+                -- Lesser used LSP functionality
+                nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                nmap('<leader>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, '[W]orkspace [L]ist Folders')
+
+                -- Create a command `:Format` local to the LSP buffer
+                vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+                    vim.lsp.buf.format()
+                end, { desc = 'Format current buffer with LSP' })
+
+                if client.server_capabilities.documentFormattingProvider then
+                    vim.api.nvim_command [[augroup Format]]
+                    vim.api.nvim_command [[autocmd! * <buffer>]]
+                    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+                    vim.api.nvim_command [[augroup END]]
+                end
+
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    pattern = '*.go',
+                    callback = function()
+                        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+                    end
+                })
+            end
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+            local mason_lspconfig = require 'mason-lspconfig'
+
+            mason_lspconfig.setup {
+                ensure_installed = vim.tbl_keys(servers),
+            }
+
+            mason_lspconfig.setup_handlers {
+                function(server_name)
+                    require('lspconfig')[server_name].setup {
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = servers[server_name],
+                    }
+                end,
+            }
+        end
     }
 }
