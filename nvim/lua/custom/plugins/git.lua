bfd4af50bda35d5cd23f22e9feb3211c8e43336a
Paul Thomson <pthomson@nurturecloud.com>
2025-02-14 15:31:32 +1100

    Add advanced-git-search nvim plugin

--- nvim/lua/custom/plugins/git.lua
+++ nvim/lua/custom/plugins/git.lua
@@ -36,4 +36,21 @@ return {
             "nvim-lua/plenary.nvim",
         },
     },
+    {
+        "aaronhallaert/advanced-git-search.nvim",
+        cmd = { "AdvancedGitSearch" },
+        config = function()
+            require("telescope").load_extension("advanced_git_search")
+        end,
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            -- to show diff splits and open commits in browser
+            "tpope/vim-fugitive",
+            -- to open commits in browser with fugitive
+            "tpope/vim-rhubarb",
+            -- optional: to replace the diff from fugitive with diffview.nvim
+            -- (fugitive is still needed to open in browser)
+            -- "sindrets/diffview.nvim",
+        },
+    }
 }
--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -21,6 +21,49 @@ return {
                         return { "--hidden" }
                     end
                 },
+            },
+            extensions = {
+                advanced_git_search = {
+                    -- Browse command to open commits in browser. Default fugitive GBrowse.
+                    -- {commit_hash} is the placeholder for the commit hash.
+                    browse_command = "GBrowse {commit_hash}",
+                    -- when {commit_hash} is not provided, the commit will be appended to the specified command seperated by a space
+                    -- browse_command = "GBrowse",
+                    -- => both will result in calling `:GBrowse commit`
+
+                    -- fugitive or diffview
+                    diff_plugin = "fugitive",
+                    -- customize git in previewer
+                    -- e.g. flags such as { "--no-pager" }, or { "-c", "delta.side-by-side=false" }
+                    git_flags = {},
+                    -- customize git diff in previewer
+                    -- e.g. flags such as { "--raw" }
+                    git_diff_flags = {},
+                    git_log_flags = {},
+                    -- Show builtin git pickers when executing "show_custom_functions" or :AdvancedGitSearch
+                    show_builtin_git_pickers = false,
+                    entry_default_author_or_date = "author", -- one of "author" or "date"
+                    keymaps = {
+                        -- following keymaps can be overridden
+                        toggle_date_author = "<C-w>",
+                        open_commit_in_browser = "<C-o>",
+                        copy_commit_hash = "<C-y>",
+                        show_entire_commit = "<C-e>",
+                    },
+
+                    -- Telescope layout setup
+                    telescope_theme = {
+                        function_name_1 = {
+                            -- Theme options
+                        },
+                        function_name_2 = "dropdown",
+                        -- e.g. realistic example
+                        show_custom_functions = {
+                            layout_config = { width = 0.4, height = 0.4 },
+                        },
+
+                    }
+                }
             }
         }
     },
