f6668e34f3f4386c148c68d716ee86672832aaf3
Paul Thomson <thomsonp83@gmail.com>
2025-04-22 21:32:17 +1000

    Use gemini 2.5 pro

--- nvim/lua/custom/plugins/codecompanion.lua
+++ nvim/lua/custom/plugins/codecompanion.lua
@@ -14,6 +14,17 @@ return {
                     adapter = "gemini",
                 }
             },
+            adapters = {
+                gemini = function()
+                    return require("codecompanion.adapters").extend("gemini", {
+                        schema = {
+                            model = {
+                                default = "gemini-2.5-pro-exp-03-25",
+                            },
+                        },
+                    })
+                end,
+            }
         },
     },
 }
