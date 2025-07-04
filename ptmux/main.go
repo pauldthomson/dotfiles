f3a77a56cfe67aa5eef5eb5cf58a663616de06dc
Paul Thomson <thomsonp83@gmail.com>
2025-07-05 09:02:39 +1000

    feat: add gemini window to ptmux

--- ptmux/main.go
+++ ptmux/main.go
@@ -89,6 +89,16 @@ func main() {
 				log.Fatal(err)
 			}
 
+			err = runCmd("tmux", "new-window", "-n", "gemini", "-t", projectName, "-c", startDir)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = runCmd("tmux", "send-keys", "-t", fmt.Sprintf("%s:4", projectName), "gemini", "C-m")
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			err = runCmd("tmux", "new-window", "-n", "scratch", "-t", projectName, "-c", startDir)
 			if err != nil {
 				log.Fatal(err)
