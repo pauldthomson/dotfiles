56289f5b87e0d3dd32342fc32a45d7af1249eba3
Paul Thomson <pthomson@nurturecloud.com>
2025-08-06 09:11:26 +1000

    Add fff.nvim

---
+++ nvim/lua/custom/plugins/fff.lua
@@ -0,0 +1,13 @@
+return {
+    "dmtrKovalenko/fff.nvim",
+    build = "cargo build --release",
+    keys = {
+        {
+            "ff",
+            function()
+                require("fff").find_files()
+            end,
+            desc = "Open file picker",
+        },
+    },
+}
