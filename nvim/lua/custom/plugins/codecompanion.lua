36ed8d0cfc9e7aac99b60bb8b304e036ea0d3abe
Paul Thomson <thomsonp83@gmail.com>
2025-08-24 20:56:20 +1000

    Remove some unused plugins

--- nvim/lua/custom/plugins/codecompanion.lua
+++ nvim/lua/custom/plugins/codecompanion.lua
@@ -1,75 +1,75 @@
 return {
-    {
-        "olimorris/codecompanion.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "ravitemer/mcphub.nvim",
-        },
-        opts = {
-            strategies = {
-                chat = {
-                    adapter = "anthropic",
-                },
-                inline = {
-                    adapter = "anthropic",
-                },
-            },
-            adapters = {
-                anthropic = function()
-                    return require("codecompanion.adapters").extend("anthropic", {
-                        schema = {
-                            model = {
-                                default = "claude-sonnet-4-20250514"
-                            },
-                        },
-                        env = {
-                            api_key = "cmd:op read \"op://private/Anthropic API Key/credential\" --no-newline"
-                        }
-                    })
-                end,
-            },
-            extensions = {
-                mcphub = {
-                    callback = "mcphub.extensions.codecompanion",
-                    opts = {
-                        show_result_in_chat = true, -- Show mcp tool results in chat
-                        make_vars = true,           -- Convert resources to #variables
-                        make_slash_commands = true, -- Add prompts as /slash commands
-                    }
-                }
-            },
-        },
-        init = function()
-            require("custom.codecompanion.spinner"):init()
-        end,
-    },
-    {
-        "ravitemer/mcphub.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-        config = function()
-            require("mcphub").setup()
-        end
-    },
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = { "markdown", "codecompanion" }
-    },
-    {
-        {
-            "HakonHarnes/img-clip.nvim",
-            opts = {
-                filetypes = {
-                    codecompanion = {
-                        prompt_for_file_name = false,
-                        template = "[Image]($FILE_PATH)",
-                        use_absolute_path = true,
-                    },
-                },
-            },
-        },
-    }
+    --     {
+    --         "olimorris/codecompanion.nvim",
+    --         dependencies = {
+    --             "nvim-lua/plenary.nvim",
+    --             "nvim-treesitter/nvim-treesitter",
+    --             "ravitemer/mcphub.nvim",
+    --         },
+    --         opts = {
+    --             strategies = {
+    --                 chat = {
+    --                     adapter = "anthropic",
+    --                 },
+    --                 inline = {
+    --                     adapter = "anthropic",
+    --                 },
+    --             },
+    --             adapters = {
+    --                 anthropic = function()
+    --                     return require("codecompanion.adapters").extend("anthropic", {
+    --                         schema = {
+    --                             model = {
+    --                                 default = "claude-sonnet-4-20250514"
+    --                             },
+    --                         },
+    --                         env = {
+    --                             api_key = "cmd:op read \"op://private/Anthropic API Key/credential\" --no-newline"
+    --                         }
+    --                     })
+    --                 end,
+    --             },
+    --             extensions = {
+    --                 mcphub = {
+    --                     callback = "mcphub.extensions.codecompanion",
+    --                     opts = {
+    --                         show_result_in_chat = true, -- Show mcp tool results in chat
+    --                         make_vars = true,           -- Convert resources to #variables
+    --                         make_slash_commands = true, -- Add prompts as /slash commands
+    --                     }
+    --                 }
+    --             },
+    --         },
+    --         init = function()
+    --             require("custom.codecompanion.spinner"):init()
+    --         end,
+    --     },
+    --     {
+    --         "ravitemer/mcphub.nvim",
+    --         dependencies = {
+    --             "nvim-lua/plenary.nvim",
+    --         },
+    --         build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    --         config = function()
+    --             require("mcphub").setup()
+    --         end
+    --     },
+    --     {
+    --         "MeanderingProgrammer/render-markdown.nvim",
+    --         ft = { "markdown", "codecompanion" }
+    --     },
+    --     {
+    --         {
+    --             "HakonHarnes/img-clip.nvim",
+    --             opts = {
+    --                 filetypes = {
+    --                     codecompanion = {
+    --                         prompt_for_file_name = false,
+    --                         template = "[Image]($FILE_PATH)",
+    --                         use_absolute_path = true,
+    --                     },
+    --                 },
+    --             },
+    --         },
+    --     }
 }
